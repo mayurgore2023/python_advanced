10133f1620eb5b6d57f612450f5ff550f7897388
MAYUR RAJENDRA GORE <125110603+mayurgore2023@users.noreply.github.com>
2023-02-20 19:36:03 +0530

    Banking application (using oop)

---
+++ oop1.py
@@ -0,0 +1,85 @@
+
+### Banking Application:
+
+# Instance variable : Name, Amount, Address, AccountNo
+# Instance method : CreateAccount(), DisplayAccountInfo()
+# Class variable : Bank_Name ,ROI_On_FD
+# Class method : DisplayBankInformation
+# Static method : DisplayKYCInfo
+
+class Bank_Account:
+
+    Bank_Name = "HDFC Bank PVT LTD"
+    ROI = 6.7
+
+    def __init__(self):
+        self.Name = " "
+        self.Amount = 0
+        self.Address = " "
+        self.AccountNo = 0
+
+    def CreatAccount(self):
+        print("Enter Your Name:")
+        self.Name = input()
+
+        print("Enter your initial ammount:")
+        self.Amount = int(input())
+
+        print("Enter your Address:")
+        self.Address = input()
+
+        print("Enter your Account Number:")
+        self.AccountNo = int(input())
+
+
+    def DisplayAccountInfo(self):
+        print("--------Your Account information below------------")
+
+        print("Name of account holder",self.Name)
+        print("Account Number:",self.AccountNo)
+        print("Address:",self.Address)
+        print("Current Account Balance:",self.Amount)
+
+    @classmethod
+    def DisplayBankInfo(cls):
+        print("Welcome to banking console")
+        print("Name of our bank is:",cls.Bank_Name)
+        print("ROI:",cls.ROI)
+
+    @staticmethod
+    def DisplayKYCinfo():
+        print("Please consider below KYC information")
+        print("According to the rules of Goverment of India you have to submit below documnets")
+        print("1 : Clear and recent passport size photo")
+        print("2 : Photo of aadhar card")
+        print("3 : Photo of PAN card")
+
+
+def main():
+
+    Bank_Account.DisplayKYCinfo()
+
+    print("Name of BAnk:",Bank_Account.Bank_Name)
+    print("Rate of interest:",Bank_Account.ROI)
+
+
+    Bank_Account.DisplayBankInfo()
+
+    User1 = Bank_Account()
+    User2 = Bank_Account()
+
+
+
+    print("Creating first account:")
+    User1.CreatAccount()
+
+    print("Creat second account:")
+    User2.CreatAccount()
+
+    User1.DisplayAccountInfo()
+    User2.DisplayAccountInfo()
+
+if __name__=="__main__":
+    main()
+
+
