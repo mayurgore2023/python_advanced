78147b47c446a9ee6c38d6d3534b4441a920c92a
MAYUR RAJENDRA GORE <125110603+mayurgore2023@users.noreply.github.com>
2023-02-20 17:43:28 +0530

    power of 2

---
+++ lambda1.py
@@ -0,0 +1,14 @@
+
+# program return the power of two  of given number from user using lambda function :
+
+def main():
+    
+    print("Poewr of Two of given number")
+    power_Function=lambda A : A**2
+    print("Enter the number:")
+    No=int(input())
+    Power=power_Function(No)
+    print("power of given number is",Power)
+
+if __name__=="__main__":
+    main()
