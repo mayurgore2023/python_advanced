f8f8fbc8385c0c8538b9620ee4d99625fbd88321
MAYUR RAJENDRA GORE <125110603+mayurgore2023@users.noreply.github.com>
2023-02-20 18:30:43 +0530

    normal FMR+ labda operations

---
+++ FMR_lambda2..py
@@ -0,0 +1,20 @@
+# Demonstration of Filter Map Reduce using lambda function
+
+from functools import reduce
+
+
+def main():
+    Arr = [8, 9, 5, 16, 2, 4, 21, 30, 11]
+
+    EvenArr= list(filter(lambda no:(no % 2 == 0),Arr))
+    print("Data after using filter :",EvenArr)
+
+    ModArray= list(map(lambda no : (no +2),EvenArr))
+    print("Data after using map :",ModArray)
+
+    sum= reduce(lambda a,b :(a + b),ModArray)
+    print(" Data after reduce:",sum)
+
+
+if __name__=="__main__":
+    main()
