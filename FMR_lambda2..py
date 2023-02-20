34ee708dd1e62c005a99f9329fcde3caddff69d5
MAYUR RAJENDRA GORE <125110603+mayurgore2023@users.noreply.github.com>
2023-02-20 18:31:41 +0530

    noemal FMR + lambda operations

--- FMR_lambda2..py
+++ FMR_lambda2..py
@@ -1,9 +1,10 @@
-# Demonstration of Filter Map Reduce using lambda function
+##  Demonstration of Filter Map Reduce using lambda function
 
 from functools import reduce
 
 
 def main():
+    
     Arr = [8, 9, 5, 16, 2, 4, 21, 30, 11]
 
     EvenArr= list(filter(lambda no:(no % 2 == 0),Arr))
